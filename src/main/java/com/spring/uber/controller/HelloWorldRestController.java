3c758d8c6c067ac681994cbabe4842a4905cdba7
smartlin5228 <smartlin1@gmail.com>
2016-11-27 20:04:59 -0500

    Initial Commit of Uber Service Hello World

---
+++ src/main/java/com/spring/uber/controller/HelloWorldRestController.java
@@ -0,0 +1,22 @@
+package com.spring.uber.controller;
+
+import com.spring.uber.domain.Message;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldRestController {
+
+    @RequestMapping("/")
+    public String welcome() {
+        return "Hello Spring Uber";
+    }
+
+    @RequestMapping(value = "/hello/{yourName}", method = RequestMethod.GET)
+    public Message showMessage(@PathVariable String yourName) {
+        Message msg = new Message(yourName, "Hello " + yourName);
+        return msg;
+    }
+}
